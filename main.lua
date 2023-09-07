6d01434d17b55f437bff778ffed849d91a70e754
MatheusMorata <matlucar09@hotmail.com>
2023-09-07 13:43:02 -0300

    Versao windows concluida

--- main.lua
+++ main.lua
@@ -1,21 +1,54 @@
 -- Aqui importo a biblioteca
 require 'md5'
 
-local op = 0
-
-while op ~= 1 do  
-    print("=====================")
-    print("[1] - Criptografar")
-    print("=====================")
-    op = tonumber(io.read())
-
+function criptografar() 
+    print("Digite uma string para criptografar: ")
+    String = io.read()
+    nova_string = "Hash de " .. String .. ": " .. md5.sumhexa(String) .. "\n"
+    local arquivo, erro = io.open("arquivo.txt", "a")
+    if arquivo then
+        arquivo:write(nova_string)
+    end 
+    arquivo:close()
     --Este comando funciona apenas no windows
-    os.execute("cls")
-    if op == 1 then
-        print("Digite uma string para criptografar: ")
-        String = io.read()
-        nova_string = md5.sumhexa(String)
-        os.execute("cls")
-        print("String criptografada: " .. nova_string)
+    os.execute("cls")   
+end
+
+function exibirHash() 
+    local arquivo = io.open("arquivo.txt", "r")
+    if arquivo then
+        -- Lê o conteúdo do arquivo e o imprime linha por linha
+        for linha in arquivo:lines() do
+            print(linha)
+        end
+        arquivo:close()
+    else 
+        print("Arquivo nao existe")
     end
 end
+
+function menu()
+    local op = 0
+    while op ~= 3 do  
+        print("=====================")
+        print("[1] - Criptografar")
+        print("[2] - Exibir as hash do arquivo")
+        print("[3] - Sair")
+        print("=====================")
+        op = tonumber(io.read())
+
+        --Este comando funciona apenas no windows
+        os.execute("cls")
+        if op == 1 then
+            criptografar()
+        elseif op == 2 then 
+            exibirHash()
+        elseif op == 3 then
+            print("Encerrando...")
+        else
+            print("Opcao invalida") 
+        end
+    end
+end
+
+menu()
